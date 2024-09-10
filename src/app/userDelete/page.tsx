1826950844e67692a6f1345a2c23e9fd1031b65c
Mr.chu <chuhongkyu@gmail.com>
2024-09-10 22:33:42 +0900

    Add GameCenter Data

--- src/app/userDelete/page.tsx
+++ src/app/userDelete/page.tsx
@@ -26,6 +26,15 @@ export default function Page() {
           <li>Follow the on-screen instructions to delete your Play Games data.</li>
         </ol>
 
+        <h2>How to Delete Your Apple Game Center Account Data</h2>
+        <p>To delete your Apple Game Center account data, please follow these steps:</p>
+        <ol>
+          <li>Open the Settings app on your iOS device.</li>
+          <li>Scroll down and select &quot;Game Center&quot;.</li>
+          <li>Select &quot;Apple ID&quot;, then choose &quot;Sign Out&quot; to remove your Game Center account.</li>
+          <li>Contact Apple Support if you wish to request deletion of all associated game data.</li>
+        </ol>
+
         <h2>Data Types Deleted or Retained</h2>
         <p>Data types that will be deleted: game progress, achievements, leaderboards.</p>
         <p>Retention period: up to 30 days as required by law.</p>
